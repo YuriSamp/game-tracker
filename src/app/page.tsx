3e4732bd5be109accbe1080511835edb6035f609
YuriSamp <yurisamp123@gmail.com>
2023-06-23 12:39:16 -0300

    feat: change header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
   }
 
   const genres = useMemo(() => {
-    return Array.from(new Set(games.map(game => game.genre)))
+    return ['', ...Array.from(new Set(games.map(game => game.genre)))]
   }, [games])
 
   const filteredGames = useMemo(() => {
@@ -50,53 +50,59 @@ export default function Home() {
 
   return (
     <main className={darkMode ? 'dark' : ''}>
-      <div className='flex flex-col items-center min-h-screen text-black dark:text-white  bg-[#f9f5f2]  dark:bg-[#020212] relative'>
-        <div className='py-6 w-ful sm:px-48 flex self-cente sm:self-end r'>
+      <div className='flex flex-col items-center min-h-screen text-black dark:text-white  bg-[#f9f5f2]  dark:bg-gray-800  relative'>
+        <div className='py-6 w-full sm:px-48 flex justify-between'>
+          <span className='text-pink-500 font-Heading text-3xl'>GAME<span className='text-blue-500'> TRACKER</span></span>
           <Switch setDarkMode={setDarkMode} />
         </div>
-        <h1 className='flex flex-col text-center text-4xl sm:text-6xl md:text-7xl  xl:text-8xl font-Heading text-red-500'>
-          <span>WELCOME <span className='text-yellow-500'>TO MY</span></span>
-          <span className='text-pink-500'>GAME<span className='text-blue-500'> TRACKER</span></span>
-        </h1>
-        <div className='hidden md:flex items-center gap-1 py-8'>
+        <div className='hidden md:flex items-center gap-1 mt-8'>
           <div className="pacman absolute right-10">
             <div className={`${darkMode ? 'pacman__mouth__dark' : 'pacman__mouth'}`}></div>
           </div>
           <div className='flex gap-10'>
-            {pacmanEats.map((_, i) => (<div key={i} className='w-6 h-6 bg-black dark:bg-white rounded-full'></div>))}
+            {pacmanEats.map((_, i) => (<div key={i} className='w-6 h-6 bg-black dark:bg-white rounded-full mt-3'></div>))}
           </div>
         </div>
-        <section className='flex flex-col gap-8 items-center'>
-          <h2 className='sm:text-xl text-center'>Insert the game you want to search</h2>
+        <h1 className='flex gap-5 text-center text-3xl sm:text-4xl md:text-5xl  xl:text-6xl font-Heading mt-5 '>
+          <span className='text-red-500' >Find </span>
+          <span className='text-orange-500' > your </span>
+          <span className='text-green-500' >favorite </span>
+          <span className='text-yellow-500'>game</span>
+        </h1>
+        <section className='flex flex-col gap-8 items-center mt-12 w-full'>
           <input
-            className='w-60 sm:w-96 border border-black dark:border-white rounded-xl bg-transparent h-10 px-5'
+            className='w-60 sm:w-1/2 border border-black dark:border-transparent  dark:bg-[#f9f5f2]  text-neutral-700 rounded-3xl bg-transparent h-14 px-5 outline-none text-2xl '
+            placeholder='Fall guys'
             value={searchTerm}
             onChange={setSearch}
+            autoFocus={true}
           />
           <div className='flex flex-col sm:flex-row gap-5 sm:gap-10'>
             <select
-              className='py-6 bg-transparent w-40 border rounded-xl px-6 border-black dark:border-white'
+              className='py-3 bg-transparent w-40 border rounded-xl px-6 dark:bg-[#f9f5f2]  bg-[#020212] text-black border-black dark:border-transparent '
               onChange={(e) => handleSelectGenre(e.target.value)}
+              value={filteredGenre}
             >
-              {genres.map(genre => <option
-                key={genre}
-                value={genre}
-                className='text-black'
-              >
-                {genre}
-              </option>)}
+              {genres.map(genre =>
+                <option
+                  key={genre}
+                  value={genre}
+                  className='text-black'
+                >
+                  {genre ? genre : 'Genre'}
+                </option>)}
             </select>
             <button
               onClick={reset}
-              className='py-6 bg-transparent w-40 border border-black dark:border-white rounded-xl px-6'
+              className='py-3 bg-transparent w-40 border dark:bg-[#f9f5f2]  bg-[#020212] text-black rounded-xl px-6 border-black dark:border-transparent '
             >
               Reset
             </button>
           </div>
         </section>
         {!Boolean(errorMensage) ?
-          <section className='pt-40 pb-32 px-20 grid gap-y-10 sm:grid-cols-2 xl:grid-cols-3 justify-items-center w-full '>
-            {filteredGames.slice(0, 21).map(item => (
+          <section className='mt-16 mb-32 px-20 grid gap-y-10 sm:grid-cols-2 xl:grid-cols-3 justify-items-center w-full '>
+            {filteredGames.map(item => (
               <Card
                 key={item.id}
                 developer={item.developer}
@@ -109,12 +115,12 @@ export default function Home() {
             ))}
           </section>
           :
-          <section className='py-48 flex flex-col items-center gap-10'>
-            <h2 className='text-3xl font-Heading'>
+          <section className='mt-32 flex flex-col items-center text-center mx-48 gap-10'>
+            <h2 className='text-6xl font-Heading'>
               {errorMensage}
             </h2>
             <button
-              className='w-48 h-12 bg-transparent border border-white rounded-lg'
+              className='py-3 bg-transparent w-40 border dark:bg-[#f9f5f2]  bg-[#020212] text-black rounded-xl px-6 border-black dark:border-transparent'
               onClick={refetch}
             >
               Retry
